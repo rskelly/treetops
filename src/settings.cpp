51eebae7bf23095adcaf847e0b2e4f200f86e41f
rob <rob@dijital.ca>
2020-05-13 12:23:38 -0700

    Remove underscore from anon methods.

--- src/settings.cpp
+++ src/settings.cpp
@@ -22,7 +22,7 @@ namespace {
 	 * \param filename The target file. Will be overwritten.
 	 * \param map The map.
 	 */
-	bool _load(const std::string& filename, smap& map) {
+	bool loadMap(const std::string& filename, smap& map) {
 		if (filename.empty())
 			return false;
 		std::ifstream ins(filename, std::ios::in);
@@ -45,7 +45,7 @@ namespace {
 	 * \param filename The target file.
 	 * \param map The map.
 	 */
-	void _save(const std::string& filename, smap& map) {
+	void saveMap(const std::string& filename, smap& map) {
 		if (!filename.empty()) {
 			std::ofstream ofs(filename, std::ios::out);
 			if (ofs.good()) {
@@ -63,7 +63,7 @@ namespace {
 	 * \param key The key.
 	 * \param alt The alternate value.
 	 */
-	int _geti(const smap& map, const std::string& key, int alt) {
+	int geti(const smap& map, const std::string& key, int alt) {
 		if (map.find(key) == map.end()) {
 			return alt;
 		}
@@ -80,7 +80,7 @@ namespace {
 	 * \param key The key.
 	 * \param alt The alternate value.
 	 */
-	double _getf(const smap& map, const std::string& key, double alt) {
+	double getf(const smap& map, const std::string& key, double alt) {
 		if (map.find(key) == map.end()) {
 			return alt;
 		}
@@ -98,7 +98,7 @@ namespace {
 	 * \param key The key.
 	 * \param alt The alternate value.
 	 */
-	bool _getb(const smap& map, const std::string& key, bool alt) {
+	bool getb(const smap& map, const std::string& key, bool alt) {
 		if (map.find(key) == map.end()) {
 			return alt;
 		}
@@ -116,7 +116,7 @@ namespace {
 	 * \param key The key.
 	 * \param alt The alternate value.
 	 */
-	std::string _gets(const smap& map, const std::string& key, const std::string& alt) {
+	std::string gets(const smap& map, const std::string& key, const std::string& alt) {
 		if (map.find(key) == map.end()) {
 			return alt;
 		}
@@ -139,38 +139,38 @@ bool Settings::load(TreetopsConfig& config, const std::string& filename) {
 	m_settings.setValue("local/settings", QString(filename.c_str()));
 
 	smap map;
-	if(!_load(filename, map))
+	if(!loadMap(filename, map))
 		return false;
 
-	config.setBuildIndex(_getb(map, "buildIndex", config.buildIndex()));
-	config.setTableCacheSize(_geti(map, "tableCacheSize", config.tableCacheSize()));
-	config.setRowCacheSize(_geti(map, "rowCacheSize", config.rowCacheSize()));
+	config.setBuildIndex(getb(map, "buildIndex", config.buildIndex()));
+	config.setTableCacheSize(geti(map, "tableCacheSize", config.tableCacheSize()));
+	config.setRowCacheSize(geti(map, "rowCacheSize", config.rowCacheSize()));
 
-	config.setOriginalCHM(_gets(map, "originalCHM", config.originalCHM()));
-	config.setOriginalCHMBand(_geti(map, "originalCHMBand", config.originalCHMBand()));
-	config.setSmoothedCHM(_gets(map, "smoothedCHM", config.smoothedCHM()));
-	config.setSmoothedCHMDriver(_gets(map, "smoothedCHMDriver", config.smoothedCHMDriver()));
-	config.setTreetopsDatabase(_gets(map, "treetopsDatabase", config.treetopsDatabase()));
-	config.setTreetopsDatabaseDriver(_gets(map, "treetopsDatabaseDriver", config.treetopsDatabaseDriver()));
-	config.setCrownsRaster(_gets(map, "crownsRaster", config.crownsRaster()));
-	config.setCrownsRasterDriver(_gets(map, "crownsRasterDriver", config.crownsRasterDriver()));
-	config.setCrownsDatabase(_gets(map, "crownsDatabase", config.crownsDatabase()));
-	config.setCrownsDatabaseDriver(_gets(map, "crownsDatabaseDriver", config.crownsDatabaseDriver()));
+	config.setOriginalCHM(gets(map, "originalCHM", config.originalCHM()));
+	config.setOriginalCHMBand(geti(map, "originalCHMBand", config.originalCHMBand()));
+	config.setSmoothedCHM(gets(map, "smoothedCHM", config.smoothedCHM()));
+	config.setSmoothedCHMDriver(gets(map, "smoothedCHMDriver", config.smoothedCHMDriver()));
+	config.setTreetopsDatabase(gets(map, "treetopsDatabase", config.treetopsDatabase()));
+	config.setTreetopsDatabaseDriver(gets(map, "treetopsDatabaseDriver", config.treetopsDatabaseDriver()));
+	config.setCrownsRaster(gets(map, "crownsRaster", config.crownsRaster()));
+	config.setCrownsRasterDriver(gets(map, "crownsRasterDriver", config.crownsRasterDriver()));
+	config.setCrownsDatabase(gets(map, "crownsDatabase", config.crownsDatabase()));
+	config.setCrownsDatabaseDriver(gets(map, "crownsDatabaseDriver", config.crownsDatabaseDriver()));
 
-	config.setDoSmoothing(_getb(map, "doSmoothing", config.doSmoothing()));
-	config.setSmoothWindowSize(_geti(map, "smoothWindowSize", config.smoothWindowSize()));
-	config.setSmoothSigma(_getf(map, "smoothSigma", config.smoothSigma()));
+	config.setDoSmoothing(getb(map, "doSmoothing", config.doSmoothing()));
+	config.setSmoothWindowSize(geti(map, "smoothWindowSize", config.smoothWindowSize()));
+	config.setSmoothSigma(getf(map, "smoothSigma", config.smoothSigma()));
 
-	config.setDoTops(_getb(map, "doTops", config.doTops()));
-	config.parseTopsThresholds(_gets(map, "topsThresholds", ""));
-	config.setTopsMaxNulls(_getf(map, "topsMaxNulls", config.topsMaxNulls()));
+	config.setDoTops(getb(map, "doTops", config.doTops()));
+	config.parseTopsThresholds(gets(map, "topsThresholds", ""));
+	config.setTopsMaxNulls(getf(map, "topsMaxNulls", config.topsMaxNulls()));
 
-	config.setDoCrowns(_getb(map, "doCrowns", config.doCrowns()));
-	config.parseCrownsThresholds(_gets(map, "crownsThresholds", ""));
-	config.setCrownsUpdateHeights(_getb(map, "crownsUpdateHeights", config.crownsUpdateHeights()));
-	config.setCrownsDoDatabase(_getb(map, "crownsDoDatabase", config.crownsDoDatabase()));
-	config.setCrownsRemoveHoles(_getb(map, "crownsRemoveHoles", config.crownsRemoveHoles()));
-	config.setCrownsRemoveDangles(_getb(map, "crownsRemoveDangles", config.crownsRemoveDangles()));
+	config.setDoCrowns(getb(map, "doCrowns", config.doCrowns()));
+	config.parseCrownsThresholds(gets(map, "crownsThresholds", ""));
+	config.setCrownsUpdateHeights(getb(map, "crownsUpdateHeights", config.crownsUpdateHeights()));
+	config.setCrownsDoDatabase(getb(map, "crownsDoDatabase", config.crownsDoDatabase()));
+	config.setCrownsRemoveHoles(getb(map, "crownsRemoveHoles", config.crownsRemoveHoles()));
+	config.setCrownsRemoveDangles(getb(map, "crownsRemoveDangles", config.crownsRemoveDangles()));
 
 	return true;
 }
@@ -208,7 +208,7 @@ void Settings::save(TreetopsConfig& config) {
 	map["crownsRemoveHoles"] = std::to_string(config.crownsRemoveHoles());
 	map["crownsRemoveDangles"] = std::to_string(config.crownsRemoveDangles());
 
-	_save(config.settings(), map);
+	saveMap(config.settings(), map);
 }
 
 Settings::~Settings() {
