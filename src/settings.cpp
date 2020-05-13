1c1437a1a137dc922910ed2572a2bb2fdc43a2ad
Rob <rob@dijital.ca>
2020-05-13 12:08:06 -0700

    Move utils into anon namespace.

--- src/settings.cpp
+++ src/settings.cpp
@@ -14,110 +14,118 @@
 
 using namespace geo::treetops::config;
 
-/**
- * Load the key-value file contents into a map.
- *
- * \param filename The target file. Will be overwritten.
- * \param map The map.
- */
-bool _load(const std::string& filename, smap& map) {
-	if(filename.empty())
-		return false;
-	std::ifstream ins(filename, std::ios::in);
-	if(ins.bad())
-		return false;
-	constexpr int len = 1024;
-	char key[len];
-	char val[len];
-	while(ins.good()) {
-		ins.getline(key, len, ':');
-		ins.getline(val, len, '\n');
-		map[key] = val;
-	}
-	return true;
-}
+namespace {
 
-/**
- * Save the map as a key-value file.
- *
- * \param filename The target file.
- * \param map The map.
- */
-void _save(const std::string& filename, smap& map) {
-	if(!filename.empty()) {
-		std::ofstream ofs(filename, std::ios::out);
-		if(ofs.good()) {
-			for(auto& it : map)
-				ofs << it.first << ":" << it.second << "\n";
+	/**
+	 * Load the key-value file contents into a map.
+	 *
+	 * \param filename The target file. Will be overwritten.
+	 * \param map The map.
+	 */
+	bool _load(const std::string& filename, smap& map) {
+		if (filename.empty())
+			return false;
+		std::ifstream ins(filename, std::ios::in);
+		if (ins.bad())
+			return false;
+		constexpr int len = 1024;
+		char key[len];
+		char val[len];
+		while (ins.good()) {
+			ins.getline(key, len, ':');
+			ins.getline(val, len, '\n');
+			map[key] = val;
+		}
+		return true;
+	}
+
+	/**
+	 * Save the map as a key-value file.
+	 *
+	 * \param filename The target file.
+	 * \param map The map.
+	 */
+	void _save(const std::string& filename, smap& map) {
+		if (!filename.empty()) {
+			std::ofstream ofs(filename, std::ios::out);
+			if (ofs.good()) {
+				for (auto& it : map)
+					ofs << it.first << ":" << it.second << "\n";
+			}
 		}
 	}
-}
 
-/**
- * Return the map value corresponding to the given key as an integer.
- * Return the alternate if the key doesn't exist.
- *
- * \param map The map.
- * \param key The key.
- * \param alt The alternate value.
- */
-int _geti(const smap& map, const std::string& key, int alt) {
-	if(map.find(key) == map.end()) {
-		return alt;
-	} else {
-		return atoi(map.at(key).c_str());
+	/**
+	 * Return the map value corresponding to the given key as an integer.
+	 * Return the alternate if the key doesn't exist.
+	 *
+	 * \param map The map.
+	 * \param key The key.
+	 * \param alt The alternate value.
+	 */
+	int _geti(const smap& map, const std::string& key, int alt) {
+		if (map.find(key) == map.end()) {
+			return alt;
+		}
+		else {
+			return atoi(map.at(key).c_str());
+		}
 	}
-}
 
-/**
- * Return the map value corresponding to the given key as a double.
- * Return the alternate if the key doesn't exist.
- *
- * \param map The map.
- * \param key The key.
- * \param alt The alternate value.
- */
-double _getf(const smap& map, const std::string& key, double alt) {
-	if(map.find(key) == map.end()) {
-		return alt;
-	} else {
-		return atof(map.at(key).c_str());
+	/**
+	 * Return the map value corresponding to the given key as a double.
+	 * Return the alternate if the key doesn't exist.
+	 *
+	 * \param map The map.
+	 * \param key The key.
+	 * \param alt The alternate value.
+	 */
+	double _getf(const smap& map, const std::string& key, double alt) {
+		if (map.find(key) == map.end()) {
+			return alt;
+		}
+		else {
+			return atof(map.at(key).c_str());
+		}
 	}
-}
 
-/**
- * Return the map value corresponding to the given key as a boolean.
- * Return the alternate if the key doesn't exist.
- * Allowed values are anything that starts with 't' or 'T' or '1'.
- *
- * \param map The map.
- * \param key The key.
- * \param alt The alternate value.
- */
-bool _getb(const smap& map, const std::string& key, bool alt) {
-	if(map.find(key) == map.end()) {
-		return alt;
-	} else {
-		const std::string& val = map.at(key);
-		return val[0] == 't' || val[0] == 'T' || val[0] == '1';
+	/**
+	 * Return the map value corresponding to the given key as a boolean.
+	 * Return the alternate if the key doesn't exist.
+	 * Allowed values are anything that starts with 't' or 'T' or '1'.
+	 *
+	 * \param map The map.
+	 * \param key The key.
+	 * \param alt The alternate value.
+	 */
+	bool _getb(const smap& map, const std::string& key, bool alt) {
+		if (map.find(key) == map.end()) {
+			return alt;
+		}
+		else {
+			const std::string& val = map.at(key);
+			return val[0] == 't' || val[0] == 'T' || val[0] == '1';
+		}
 	}
-}
 
-/**
- * Return the map value corresponding to the given key as a string.
- * Return the alternate if the key doesn't exist.
- *
- * \param map The map.
- * \param key The key.
- * \param alt The alternate value.
- */
-std::string _gets(const smap& map, const std::string& key, const std::string& alt) {
-	if(map.find(key) == map.end()) {
-		return alt;
-	} else {
-		return map.at(key);
+	/**
+	 * Return the map value corresponding to the given key as a string.
+	 * Return the alternate if the key doesn't exist.
+	 *
+	 * \param map The map.
+	 * \param key The key.
+	 * \param alt The alternate value.
+	 */
+	std::string _gets(const smap& map, const std::string& key, const std::string& alt) {
+		if (map.find(key) == map.end()) {
+			return alt;
+		}
+		else {
+			return map.at(key);
+		}
 	}
-}
+
+} // anon
 
 Settings::Settings() {
 	m_lastDir = m_settings.value("local/lastDir", "").toString().toStdString();
